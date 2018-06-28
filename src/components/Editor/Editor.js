52931681fb20e47567fed58c1892b837eee2dd73
Luke Peavey <1williampeavey@gmail.com>
2018-06-28 00:14:33 -0400

    Build the Editor component

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './Editor.css'
 
 /**
  * Renders the editor panel that contains text input
  */
 export default class Editor extends React.Component {
-  handleChange = (event) => {}
+  handleChange = (event) => {
+    const value = event.target.value
+    this.props.changeEditorContent(value)
+  }
 
   render() {
-    return <textarea id="editor" />
+    const { value } = this.props
+    return (
+      <section className="Editor split">
+        <textarea id="editor" value={value} onChange={this.handleChange} />
+      </section>
+    )
   }
 }
 
 Editor.propTypes = {
   /** The current content of the editor */
-  inputValue: PropTypes.string.isRequired,
+  editorContent: PropTypes.string.isRequired,
   /** Function to change the input value stored in state */
   changeEditorContent: PropTypes.func.isRequired,
 }
