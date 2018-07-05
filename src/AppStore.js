0290ca404d3266108067d664cbdd411cb9362204
Luke Peavey <1williampeavey@gmail.com>
2018-07-05 04:21:10 -0400

    Add state and actions to handle new functionality
    Adding the following functionality:
    - switch between app layouts
    - save file to disk
    - open from local disk
    - reset editor content

--- src/AppStore.js
+++ src/AppStore.js
@@ -1,24 +1,82 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import FileSaver from 'file-saver'
 import exampleDoc from './example.md'
-/**
- * A root component that serves as the store for the app.
- * The state and actions are passed to the child component
- * as props.
- */
-export default class AppStore extends React.Component {
-  // App state
-  state = {
-    /** The value of the textarea (aka markdown editor) */
-    editorContent: '',
-  }
+/** Initial state for the app */
+const initialState = {
+  /**
+   * The current input value of the editor.
+   * @type {string}
+   */
+  editorContent: '',
 
-  // functions to update the state.
+  /**
+   * The title of the markdown document.
+   * @type {string}
+   */
+  documentTitle: 'My Document',
+
+  /**
+   * Controls if title input is visible
+   * @type {boolean}
+   */
+  isEditingTitle: false,
+
+  /**
+   * The layout of the app
+   * @type {'editor' | 'preview' | 'split'}
+   */
+  activeView: 'split',
+
+  /**
+   * Controls if the fiile uploader modal is visible
+   * @type {boolean}
+   */
+  isFileUploaderOpen: false
+}
+
+export default class AppStore extends React.Component {
+  /** App State */
+  state = { ...initialState }
+
+  /** Functions to update state or perform side effect  */
   actions = {
-    /** Changes the value of the text area (editorContent) stored in state */
-    changeEditorContent: (newValue) => {
-      this.setState({ editorContent: newValue })
+    /** Updates the input value (content of the editor) */
+    setEditorContent: (newValue) => {
+      this.setState((prevState) => ({ editorContent: String(newValue) }))
     },
+
+    /** Sets the title of the document */
+    setDocumentTitle: (value) => {
+      // Remove file extension and ensure title does not exceed max length
+      const documentTitle = value.replace(/\.\w+$/, '').slice(0, 30)
+      this.setState({ documentTitle })
+    },
+
+    /** Starts a new document */
+    startNewDocument: (title = '', content = '') => {
+      this.actions.setDocumentTitle(title)
+      this.actions.setEditorContent(content)
+    },
+
+    /** Sets the active view setting ('editor' | 'preview' | 'split') */
+    setActiveView: (view) => () => {
+      if (/(preview|editor|split)/.test(view)) {
+        this.setState({ activeView: view })
+      }
+    },
+
+    /** Saves the content of the editor as a markdown file to local disk  */
+    saveAsFile: () => {
+      const { editorContent, documentTitle } = this.state
+      const fileOptions = { type: 'text/plain;charset=utf-8' }
+      const file = new File([editorContent], `${documentTitle}.md`, fileOptions)
+      FileSaver.saveAs(file)
+    },
+
+    /** Toggle the file uploader modal  */
+    toggleFileUploader: (open = true) => () => {
+      this.setState({ isFileUploaderOpen: Boolean(open) })
+    }
   }
 
   async componentDidMount() {
@@ -32,8 +90,4 @@ export default class AppStore extends React.Component {
     const { state, actions } = this
     return this.props.children({ state, actions })
   }
-}
-
-AppStore.propsTypes = {
-  children: PropTypes.element.isRequired,
 }
